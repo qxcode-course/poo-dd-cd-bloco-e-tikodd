40f762fa002cdd47fa41abf36e5bd7463019a87e
tikodd <tiagocm1085@gmail.com>
2025-12-07 23:04:07 +0000

    1h fazendo zoo

---
+++ tasks/poo/zoo/py/draft.py
@@ -0,0 +1,35 @@
+Class Animal(ABC):
+    def __init__(self, nome: str):
+        self.nome = nome
+
+    def apresentar_nome(self):
+        print(f"Eu sou um(a) {self.nome}!")
+
+    def fazer_som(self):
+        pass
+
+    def mover(self):
+        pass
+
+class Leao(Animal):
+    def __init__(self, nome: str):
+        super().__init__(nome)
+
+    def fazer_som(self):
+        print("Rooaaaaar!")
+
+    def mover(self):
+        print("Oleao corre imponente pela savana.")
+Como nasjnjcnjnjdkian kajsnj lajn njcomofazer art aat
+outside bts dead chance good as we kiss didnt mas
+ascczxkkkbreathe you vscode_lsp_terminal_prompt_trackerevery move you make 
+and every step you take ill be watching you every im 
+ive been watching you cant u see
+u belong to me every move you makeevery smile you fake
+since you dont 
+only see a fraid
+for you and makevery step you take i have been watchingyou
+ill be watching you
+evey move you make
+ill be watching you
+every you make
